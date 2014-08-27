2da301c0f0dbd096bd75ade09a87d0f25d1ccddc
Paul Young <paulyoungonline@gmail.com>
2014-08-27 00:09:25 -0400

    Initial Commit

---
+++ WebSocketTransport/WebSocketTransport.h
@@ -0,0 +1,19 @@
+//
+//  WebSocketTransport.h
+//  WebSocketTransport
+//
+//  Created by Paul Young on 27/08/2014.
+//  Copyright (c) 2014 CocoaFlow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for WebSocketTransport.
+FOUNDATION_EXPORT double WebSocketTransportVersionNumber;
+
+//! Project version string for WebSocketTransport.
+FOUNDATION_EXPORT const unsigned char WebSocketTransportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebSocketTransport/PublicHeader.h>
+
+
