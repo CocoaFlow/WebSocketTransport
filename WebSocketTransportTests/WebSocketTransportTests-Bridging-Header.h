06bac912962a482f65d84552533bd74e8d128083
Paul Young <paulyoungonline@gmail.com>
2014-08-31 23:23:02 -0400

    Add bridging header for test target.

---
+++ WebSocketTransportTests/WebSocketTransportTests-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  WebSocketTransportTests-Bridging-Header.h
+//  WebSocketTransport
+//
+//  Created by Paul Young on 31/08/2014.
+//  Copyright (c) 2014 CocoaFlow. All rights reserved.
+//
+
+#import <SocketRocket/SRWebSocket.h>
