c4d4aa52ceafd4e446d97ad2d87e0dbf96aa6bf2
Paul Young <paulyoungonline@gmail.com>
2014-09-03 02:25:28 -0400

    Add fake message receiver for testing purposes.

---
+++ WebSocketTransportTests/FakeMessageReceiver.swift
@@ -0,0 +1,27 @@
+//
+//  FakeMessageReceiver.swift
+//  WebSocketTransport
+//
+//  Created by Paul Young on 03/09/2014.
+//  Copyright (c) 2014 CocoaFlow. All rights reserved.
+//
+
+import Foundation
+import MessageTransfer
+import JSONLib
+
+class FakeMessageReceiver: MessageReceiverWorkaround {
+    
+    typealias Verification = (channel: String, topic: String, payload: JSON) -> Void
+    
+    private let verify: Verification
+    var messageSender: MessageSender?
+    
+    init(_ verify: Verification = { (channel, topic, payload) in }) {
+        self.verify = verify
+    }
+    
+    func receive(channel: String, _ topic: String, _ payload: JSON) {
+        self.verify(channel: channel, topic: topic, payload: payload)
+    }
+}
