4e749aba8ccdd5812bbf9fa61ec2b54ad063abe5
Paul Young <paulyoungonline@gmail.com>
2014-09-05 14:02:51 -0400

    Remove old bridging header.

--- WebSocketTransportTests/WebSocketTransportTests-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  WebSocketTransportTests-Bridging-Header.h
-//  WebSocketTransport
-//
-//  Created by Paul Young on 31/08/2014.
-//  Copyright (c) 2014 CocoaFlow. All rights reserved.
-//
-
-#import <SocketRocket/SRWebSocket.h>
